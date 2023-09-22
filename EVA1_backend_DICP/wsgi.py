b43f46e97f4e1a4e72288fef3742cbe03b6bcc09
diego-chandia <diego.chandia06@inacapmail.cl>
2023-09-22 11:01:25 -0300

    Main Creada

---
+++ EVA1_backend_DICP/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for EVA1_backend_DICP project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EVA1_backend_DICP.settings')
+
+application = get_wsgi_application()
